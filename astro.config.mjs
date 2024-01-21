b0d00f5faaf2daac5833cf02a86bdfcff143266f
Thomas Mol <email@thomasmol.com>
2024-01-21 20:37:24 +0100

    Update astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import svelte from "@astrojs/svelte";
 import keystatic from "@keystatic/astro";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react(), markdoc(), keystatic()],
   output: "hybrid",
   site: "https://thomasmol.com",
-  adapter: vercel(),
+  adapter: vercel()
 });
